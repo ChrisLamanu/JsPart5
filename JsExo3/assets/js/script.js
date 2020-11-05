0c0f33771b9279e1b2cb02b737e806c91053b105
ChrisLamanu <christopher.vrel@gmail.com>
2020-11-05 17:41:36 +0100

    "Exo 3 corrected"

--- JsExo3/assets/js/script.js
+++ JsExo3/assets/js/script.js
@@ -1,12 +1,19 @@
+document.getElementById("errorMail").style.display = "none";
+document.getElementById("errorName").style.display = "none";
+document.getElementById("errorAge").style.display = "none";
+
 function updateMail() {
     let mail = document.getElementById("mail");
-    let regexMail = new RegExp(/^[A-Za-z][A-Za-z0-9!@#$%^&*]*$/);
+    // let regexMail = new RegExp(/^[A-Za-z][A-Za-z0-9!@#$%^&*]*$/);
+    let regexMail = new RegExp(/^[a-z0-9.-]+[@]{1}[a-z0-9]+[.]{1}[a-z]{2,4}$/);
 
     if (regexMail.test(mail.value)){
         mail.style.borderColor = "green";
+        document.getElementById("errorMail").style.display = "none";
     }
     else {
         mail.style.borderColor = "red";
+        document.getElementById("errorMail").style.display = "block";
     } 
 }
 function updateName() {
@@ -15,9 +22,11 @@ function updateName() {
 
     if (regexName.test(name.value)){
         name.style.borderColor = "green";
+        document.getElementById("errorName").style.display = "none";
     }
     else {
         name.style.borderColor = "red";
+        document.getElementById("errorName").style.display = "block";
     } 
 }
 function updateAge() {
@@ -26,8 +35,10 @@ function updateAge() {
 
     if (regexAge.test(age.value)){
         age.style.borderColor = "green";
+        document.getElementById("errorAge").style.display = "none";
     }
     else {
         age.style.borderColor = "red";
+        document.getElementById("errorAge").style.display = "block";
     } 
 }
