25b00cbd85ac908a0e53dade9975a0583b3a9257
ChrisLamanu <christopher.vrel@gmail.com>
2020-11-06 09:11:27 +0100

    "Exo 4"

---
+++ JsExo4/assets/js/script.js
@@ -0,0 +1,19 @@
+let imgSwitch;
+
+function imageSwitch() {
+    let img = document.getElementsByClassName("imgContainer");
+
+    let imgSrc = "assets/img/avengers-gameverse-marvel-626-iron-man-1-1596029401_0x460.jpg";
+    let imgSrc_b = "assets/img/my-hero-academia-803-katsuki-bakugo-1-1595931222_0x460.jpg";
+
+    if (imgSwitch) {
+        // console.log("Activation");
+        img.item(0).setAttribute("style", `background-image: url( ${imgSrc} );`);
+        imgSwitch = false;
+    }
+    else {
+        // console.log("Desactivation");
+        img.item(0).setAttribute("style", `background-image: url( ${imgSrc_b} );`);
+        imgSwitch = true;
+    }
+}
